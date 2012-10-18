e0a756a619eeb37e2a25787aeb2d8dee7e722bbd
Rafał Łasocha <me+dev@swistak35.com>
2012-10-18 22:47:53 +0200

    added expiration date to cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,7 @@ class ApplicationController < ActionController::Base
     	@current_buyer = Buyer.find(cookies[:user_id])
     else
     	@current_buyer = Buyer.create
-    	cookies[:user_id] = @current_buyer.id
+    	cookies[:user_id] = { value: @current_buyer.id, expires: 1.week.from_now }
     end
     @current_buyer
   end

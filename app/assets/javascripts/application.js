997fb777e362a83f144c599b6826ed7be9b6d02d
Rafał Łasocha <me+dev@swistak35.com>
2012-10-18 09:07:12 +0200

    added twitter bootstrap, first layout

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= bootstrap.min
 //= require_tree .
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,11 @@
 class CategoriesController < ApplicationController
+	def index
+	end
+
 	def show
+	end
+
+	def find_category
 		@category = Category.find(params[:id])
 	end
 end

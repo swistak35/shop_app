619e2d574bc3a91f072862df5b272bb9991c94d0
Rafał Łasocha <me+dev@swistak35.com>
2012-10-23 16:46:11 +0200

    added required: true to last_name

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,9 +7,4 @@ class Product < ActiveRecord::Base
 	scope :sorted, lambda { order("name ASC") }
 
 	validates :name, :desc, :price, presence: true
-
-	# validates :price, format: {
-	# 	with: /\A\d+\.\d(\d)\z/,
-	# 	message: "has invalid format. Format should be for ex. '13.45' or '3.0'."
-	# }
 end

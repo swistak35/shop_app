5f43253345fc6c9e7fe296ed77b7e46d1f1c80a2
Rafał Łasocha <me+dev@swistak35.com>
2012-10-29 10:14:06 +0100

    closes #3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,4 +29,16 @@ module ApplicationHelper
   def nice_datetime(datetime)
     datetime.strftime("%d-%m-%Y %H:%M")
   end
+
+  def basket_li(item)
+    str = ""
+    str += "#{item.quantity}x " if item.quantity > 1
+    str += "#{item.product_name}"
+    if item.quantity > 1
+      str += " (#{item.price}$ each)"
+    else
+      str += " (#{item.price}$)"
+    end
+    str
+  end
 end
